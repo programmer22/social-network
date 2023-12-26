7a4ede3aee013700d36b9b2bab8e487d12418fb7
Nick Lopacki <nlopacki@psnamericas.com>
2023-12-26 17:21:06 -0600

    changed can't to cannot and you're to you are

--- components/profile/news_section/NewsSection.tsx
+++ components/profile/news_section/NewsSection.tsx
@@ -5,7 +5,7 @@ const NewsSection = () => {
     <div className="w-full md:w-1/3 pl-4 space-y-6">
       <div className="mb-6 p-4 bg-gradient-to-r from-red-500 to-pink-500 rounded-lg shadow-lg text-white">
         <h3 className="text-xl font-bold mb-3">Post Your Event</h3>
-        <p>Have an event you would like to share? Post it on Goeventi for free! Create an account, fill out a simple form, and you're all set.</p>
+        <p>Have an event you would like to share? Post it on Goeventi for free! Create an account, fill out a simple form, and you are all set.</p>
         <button className="mt-4 bg-white text-red-500 px-4 py-2 rounded hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-red-500">Post It Now</button>
       </div>
 
@@ -13,7 +13,7 @@ const NewsSection = () => {
         <h3 className="text-lg font-semibold text-gray-800 mb-3">Latest News</h3>
         <ul className="list-disc pl-5 text-gray-600">
           <li className="mb-2">Music - Discover the latest hits and trends.</li>
-          <li className="mb-2">Ads - Exclusive deals and offers you can't miss.</li>
+          <li className="mb-2">Ads - Exclusive deals and offers you cannot miss.</li>
           <li className="mb-2">Sports - Updates on your favorite teams and matches.</li>
         </ul>
         <button className="mt-4 text-blue-500 hover:text-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">Discover More</button>
