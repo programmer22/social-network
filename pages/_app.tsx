52341c6ffebd412a40aebe1c8e55c22f1e08f771
Nick Lopacki <nlopacki@psnamericas.com>
2023-12-26 21:22:49 -0600

    changed to add ' ' at end of userEmail initialization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,19 @@
 // CreateProfilePage.tsx
-import React from "react";
-import { useUser } from "@clerk/nextjs";
-import CreateProfile from "../components/create-profile/CreateProfile";
-
-// Define the User type based on what you expect from Clerk
-interface User {
-  email: string;
-  // Include other user properties you expect
-}
+import React from 'react';
+import { useUser } from '@clerk/nextjs';
+import CreateProfile from '../components/create-profile/CreateProfile';
 
 export default function CreateProfilePage() {
   const { user } = useUser();
 
+  // Assuming 'user' is of a type where 'email' might not be a recognized property,
+  // you can use a type assertion (if you are sure the property exists):
+  const userEmail = user ? (user as any).email : '';
+
   return (
     <div className="container mx-auto px-4 py-8">
-      <CreateProfile userEmail={user?.email} />
+      <CreateProfile userEmail={userEmail} />
     </div>
   );
 }
-
 
