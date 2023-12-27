dcffca0bb99e1bf3b63631ab2eeab4ea71aa964d
Nick Lopacki <nlopacki@psnamericas.com>
2023-12-26 21:29:00 -0600

    change user object in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,13 @@ import { useUser } from '@clerk/nextjs';
 import CreateProfile from '../components/create-profile/CreateProfile';
 
 export default function CreateProfilePage() {
-  const { user } = useUser();
-
   // Assuming 'user' is of a type where 'email' might not be a recognized property,
   // you can use a type assertion (if you are sure the property exists):
-  const userEmail = user ? (user as any).email : '';
+  const { user } = useUser() as { user: { email?: string } | null };
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <CreateProfile userEmail={userEmail} />
+      <CreateProfile userEmail={user?.email} />
     </div>
   );
 }
-
