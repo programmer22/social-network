3f7c0350f798fb2d944e019081680543466d9965
Nick Lopacki <nlopacki@psnamericas.com>
2023-12-27 18:15:29 -0600

    Edited _app.tsx ClerkProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,14 @@
 // pages/_app.tsx
 import { AppProps } from 'next/app';
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider, ClerkLoaded } from '@clerk/nextjs';
 import '../app/globals.css';
-import Header from '../components/header/Header';
-import Footer from '../components/footer/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
   return (
     <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <ClerkLoaded>
+        <Component {...pageProps} />
+      </ClerkLoaded>
     </ClerkProvider>
   );
 }
