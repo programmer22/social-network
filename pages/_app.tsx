b707a5e10e9e133fcaff33b5fafc7ed5f2fe96c4
Nick Lopacki <nlopacki@psnamericas.com>
2023-12-27 18:03:43 -0600

    Edited ClerkProvider in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,14 @@
 // pages/_app.tsx
 import { AppProps } from 'next/app';
-import { ClerkProvider, ClerkProviderProps } from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';
 import '../app/globals.css';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // ClerkProviderProps should be defined according to your Clerk configuration
-  const clerkProps: ClerkProviderProps = {
-    frontendApi: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API,
-  };
-
+  
   return (
-    <ClerkProvider {...clerkProps}>
+    <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}>
       <Header />
       <Component {...pageProps} />
       <Footer />
